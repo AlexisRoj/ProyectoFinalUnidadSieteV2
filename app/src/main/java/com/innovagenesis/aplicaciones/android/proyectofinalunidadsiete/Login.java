6437edc02bb4f39967949329c38f7de09066e157
AlexisRoj <alexisroj2@gmail.com>
2017-02-12 09:14:22 -0600

    Implementando Login Activity

--- app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadsiete/Login.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadsiete/Login.java
@@ -4,12 +4,13 @@ import android.content.SharedPreferences;
 import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.innovagenesis.aplicaciones.android.proyectofinalunidadsiete.preference.PreferenceConstant;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private SharedPreferences preferences;
     private String user_name;
     private String user_pass;
 
@@ -19,15 +20,41 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        preferences = getSharedPreferences(PreferenceConstant.PREFERENCE_LOGIN,MODE_PRIVATE);
+        SharedPreferences preferences =
+                getSharedPreferences(PreferenceConstant.PREFERENCE_LOGIN, MODE_PRIVATE);
 
         /** Cargar preferencias para hacer el login*/
-        user_name = preferences.getString(PreferenceConstant.USER_NAME,null);
-        user_pass = preferences.getString(PreferenceConstant.USER_PASS,null);
+        user_name = preferences.getString(PreferenceConstant.USER_NAME, null);
+        user_pass = preferences.getString(PreferenceConstant.USER_PASS, null);
 
+        /** Castea y Activa los botones*/
 
+        Button btnIngresar = (Button) findViewById(R.id.btnIngLogin);
+        Button btnRegistrarse = (Button) findViewById(R.id.btnRegLogin);
+
+        btnIngresar.setOnClickListener(this);
+        btnRegistrarse.setOnClickListener(this);
 
         /** Cambia el titulo del login*/
         this.setTitle(getString(R.string.titleName));
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int identificador = v.getId();
+
+        switch (identificador) {
+
+            case R.id.btnIngLogin: {
+                break;
+            }
+
+            case R.id.btnRegLogin: {
+                break;
+            }
+
+        }
+
     }
 }
