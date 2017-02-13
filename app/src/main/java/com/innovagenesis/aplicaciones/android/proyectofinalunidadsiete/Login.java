8dac63f7dfc2c5fe41a4585567c3d9265bf159ec
AlexisRoj <alexisroj2@gmail.com>
2017-02-13 14:49:09 -0600

    init comit

--- app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadsiete/Login.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadsiete/Login.java
@@ -2,17 +2,31 @@ package com.innovagenesis.aplicaciones.android.proyectofinalunidadsiete;
 
 import android.content.SharedPreferences;
 import android.preference.Preference;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.innovagenesis.aplicaciones.android.proyectofinalunidadsiete.login_async.ConsultarLoginAsync;
 import com.innovagenesis.aplicaciones.android.proyectofinalunidadsiete.preference.PreferenceConstant;
 
-public class Login extends AppCompatActivity implements View.OnClickListener {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Login extends AppCompatActivity implements View.OnClickListener,
+        ConsultarLoginAsync.OnConsultarUsuarioGetAsync {
 
     private String user_name;
     private String user_pass;
+    private TextInputLayout textInputUserName;
+    private TextInputLayout textInputUserPass;
+
+    private int id_comparar;
+    private String username_comparar;
+    private String user_pass_comparar;
 
 
     @Override
@@ -35,6 +49,17 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         btnIngresar.setOnClickListener(this);
         btnRegistrarse.setOnClickListener(this);
 
+        /** Captura de datos de ingreso*/
+
+        textInputUserName = (TextInputLayout) findViewById(R.id.user_name);
+        textInputUserPass = (TextInputLayout) findViewById(R.id.user_pass);
+
+        /** Validacion de existencia del sharepreference*/
+        if (user_name != null && user_pass != null) {
+
+
+        }
+
         /** Cambia el titulo del login*/
         this.setTitle(getString(R.string.titleName));
     }
@@ -47,6 +72,43 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         switch (identificador) {
 
             case R.id.btnIngLogin: {
+                /** captura los datos si no estan en la preference*/
+
+                EditText editTextUserName = textInputUserName.getEditText();
+                EditText editTextUserPass = textInputUserPass.getEditText();
+
+                if (textInputUserName.getEditText() != null) {
+                    assert editTextUserName != null;
+                    user_name = editTextUserName.getText().toString();
+                }
+
+                if (textInputUserPass.getEditText() != null) {
+                    assert editTextUserPass != null;
+                    user_pass = editTextUserPass.getText().toString();
+                }
+
+                Boolean conectar = true;
+
+                if ("".equals(user_name))
+                    conectar = false;
+
+                if ("".equals(user_pass))
+                    conectar = false;
+
+                if (conectar) {
+                    /** Primero crea la conexion al servicio */
+                    try {
+                        new ConsultarLoginAsync(this).execute( new URL("http://192.168.100.4:3306/WebServiceExamenSiete/webapi/Users/"
+                        + user_name) );
+
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+
+
+                } else
+                    Toast.makeText(this, "No pueden haber campos vacios", Toast.LENGTH_SHORT).show();
+
                 break;
             }
 
@@ -56,5 +118,15 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         }
 
+    }
+
+    /** Variables que atrapan los datos que vienen del query*/
+
+
+    @Override
+    public void onConsultarUsuarioGetFinish(String id, String Username, String Userpass) {
+        //id_comparar = id;
+        user_pass_comparar = Username;
+        user_pass_comparar = Userpass;
     }
 }
