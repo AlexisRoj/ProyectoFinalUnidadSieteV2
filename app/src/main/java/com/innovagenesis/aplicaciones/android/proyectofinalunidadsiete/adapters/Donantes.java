9d46d551389e8ad8b23f64cc79b44d59d51d5f22
AlexisRoj <alexisroj2@gmail.com>
2017-02-27 06:53:10 -0600

    Se inicia implementacion de Donantes

---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/proyectofinalunidadsiete/adapters/Donantes.java
@@ -0,0 +1,77 @@
+package com.innovagenesis.aplicaciones.android.proyectofinalunidadsiete.adapters;
+
+/**
+ * Crea los valores a capturar en donantes
+ * Clase que ayuda a agregar los donantes
+ * Created by alexi on 27/02/2017.
+ */
+
+public class Donantes {
+
+
+    private int donante_ced;
+    private String donante_nombre;
+    private String donante_apellido;
+    private int donante_edad;
+    private String donante_tipo_sangre;
+    private int donante_peso;
+    private int donante_estatura;
+
+    public int getDonante_ced() {
+        return donante_ced;
+    }
+
+    public void setDonante_ced(int donante_ced) {
+        this.donante_ced = donante_ced;
+    }
+
+    public String getDonante_nombre() {
+        return donante_nombre;
+    }
+
+    public void setDonante_nombre(String donante_nombre) {
+        this.donante_nombre = donante_nombre;
+    }
+
+    public String getDonante_apellido() {
+        return donante_apellido;
+    }
+
+    public void setDonante_apellido(String donante_apellido) {
+        this.donante_apellido = donante_apellido;
+    }
+
+    public int getDonante_edad() {
+        return donante_edad;
+    }
+
+    public void setDonante_edad(int donante_edad) {
+        this.donante_edad = donante_edad;
+    }
+
+    public String getDonante_tipo_sangre() {
+        return donante_tipo_sangre;
+    }
+
+    public void setDonante_tipo_sangre(String donante_tipo_sangre) {
+        this.donante_tipo_sangre = donante_tipo_sangre;
+    }
+
+    public int getDonante_peso() {
+        return donante_peso;
+    }
+
+    public void setDonante_peso(int donante_peso) {
+        this.donante_peso = donante_peso;
+    }
+
+    public int getDonante_estatura() {
+        return donante_estatura;
+    }
+
+    public void setDonante_estatura(int donante_estatura) {
+        this.donante_estatura = donante_estatura;
+    }
+
+
+}
